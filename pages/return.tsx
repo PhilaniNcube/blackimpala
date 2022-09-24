4d69d73781d2121908de457baf7f27cf5428c3de
Philani Ncube <ncbphi001@gmail.com>
2022-09-24 15:05:27 +0200

    return page

--- pages/return.tsx
+++ pages/return.tsx
@@ -1,4 +1,16 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
 const ReturnUrl = () => {
-  return <div>return</div>;
+  return <Fragment>
+    <Head>
+      <title>Order Success | Black Impala</title>
+    </Head>
+    <main className="bg-slate-900 py-12 min-h-screen">
+      <div className="max-w-7xl mx-auto px-4 flex items-center justify-center">
+        <h1 className="font-bold font-brand text-white text-5xl">Payment Successful</h1>
+      </div>
+    </main>
+  </Fragment>;
 };
 export default ReturnUrl;
