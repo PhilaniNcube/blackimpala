71b2c79ff660505178154fa52b2cd771577ca2ad
Philani Ncube <ncbphi001@gmail.com>
2024-03-10 12:56:09 +0200

    Update layout.tsx in dashboard

---
+++ app/dashboard/events/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { PlusIcon } from "lucide-react";
+import Link from "next/link";
+
+const EventsPage = () => {
+  return (
+			<div>
+				<div className="flex items-center justify-between">
+					<h1 className="text-3xl font-medium">Events</h1>
+					<Link href="/dashboard/events/create">
+						<Button className="btn btn-primary"><PlusIcon />Create Event</Button>
+					</Link>
+				</div>
+        <Separator className="my-3" />
+			</div>
+		);
+};
+export default EventsPage;
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,9 +2,9 @@ import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { is_admin } from "@/lib/fetchers/auth";
 import { Separator } from "@/components/ui/separator";
 import { redirect } from "next/navigation";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import Link from "next/link";
-import { BoxIcon, ShoppingBagIcon } from "lucide-react";
+import { BoxIcon, PartyPopperIcon, ShoppingBagIcon } from "lucide-react";
 
 const DashboardLayout = async ({children}:{children:ReactNode}) => {
 
@@ -17,23 +17,33 @@ const DashboardLayout = async ({children}:{children:ReactNode}) => {
 				return (
 					<div className="flex w-full h-[calc(100vh-64px)] overflow-y-hidden">
 						<aside className="flex flex-col w-1/5 h-full text-white bg-black">
-              <h2 className="px-4 py-4 text-lg font-medium">
-                Admin Dashboard
-              </h2>
-              <Separator />
-              <div className="flex flex-col py-6 space-y-2">
-                <Link className="flex items-center w-full px-3 py-2 space-x-2 hover:text-slate-200 hover:bg-slate-800" href="/dashboard/products">
-                 <BoxIcon /> Products
-                </Link>
-                <Link className="flex items-center w-full px-3 py-2 space-x-2 hover:text-slate-200 hover:bg-slate-800" href="/dashboard/orders">
-                 <ShoppingBagIcon /> Orders
-                </Link>
-              </div>
-            </aside>
+							<h2 className="px-4 py-4 text-lg font-medium">Admin Dashboard</h2>
+							<Separator />
+							<div className="flex flex-col py-6 space-y-2">
+								<Link
+									className="flex items-center w-full px-3 py-2 space-x-2 hover:text-slate-200 hover:bg-slate-800"
+									href="/dashboard/products"
+								>
+									<BoxIcon className="pr-2" /> Products
+								</Link>
+								<Link
+									className="flex items-center w-full px-3 py-2 space-x-2 hover:text-slate-200 hover:bg-slate-800"
+									href="/dashboard/orders"
+								>
+									<ShoppingBagIcon className="pr-2" /> Orders
+								</Link>
+								<Link
+									className="flex items-center w-full px-3 py-2 space-x-2 hover:text-slate-200 hover:bg-slate-800"
+									href="/dashboard/events"
+								>
+									<PartyPopperIcon className="pr-2" /> Events
+								</Link>
+							</div>
+						</aside>
 						<div className="flex-1">
 							<ScrollArea className="h-[calc(100vh-64px)] px-4 py-5">
 								{children}
-                <ScrollBar />
+								<ScrollBar />
 							</ScrollArea>
 						</div>
 					</div>
